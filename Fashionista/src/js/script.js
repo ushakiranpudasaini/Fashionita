d9942dfd9ef91fd3b1122170f25734400c777c4a
Usha Kiran Pudasaini <90036548+ushakiranpudasaini@users.noreply.github.com>
2023-05-19 17:40:11 +0545

    Fashionita

---
+++ Fashionista/src/js/script.js
@@ -0,0 +1,46 @@
+$(window).scroll(function () {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $('#back2Top').fadeIn();
+  } else {
+    $('#back2Top').fadeOut();
+  }
+});
+
+$(document).ready(function () {
+
+  // Back To Top
+  $("#back2Top").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  });
+
+});
+
+
+(function ($) { // Begin jQuery
+  $(function () { // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $('nav ul li a:not(:only-child)').click(function (e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      // Close one dropdown when selecting another
+      $('.nav-dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $('html').click(function () {
+      $('.nav-dropdown').hide();
+    });
+    // Toggle open and close nav styles on click
+    $('#nav-toggle').click(function () {
+      $('nav ul').slideToggle();
+    });
+    // Hamburger to X toggle
+    $('#nav-toggle').on('click', function () {
+      this.classList.toggle('active');
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
